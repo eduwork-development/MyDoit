16d08234fbf3bf27660f2394e017a087237facd1
thahirudin.works <thahirudin.works>
2024-01-17 16:10:36 +0700

    rev perpajakan

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+      },
+      colors: {
+        customPrimary: {
+          500: "#00D27F",
+          600: "#00A866",
+        },
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
